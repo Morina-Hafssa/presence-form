a0a0c2959c1983dfbeb1b8e3b76438264a483acf
Morina-Hafssa <98556817+Morina-Hafssa@users.noreply.github.com>
2025-10-19 20:44:01 +0100

    Update QR code link to use GitHub Pages

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function generateNewQR() {
   const session = Math.random().toString(36).substring(2, 8);
-  const link = `https://github.com/Morina-Hafssa/presence-form/form.html?session=${session}`;
+  const link = `https://morina-hafssa.github.io/presence-form/form.html?session=${session}`;
   QRCode.toCanvas(document.getElementById('qrcode'), link, (err) => {
     if (err) console.error(err);
   });
